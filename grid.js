66a7c1ee51c505ab2b15b1158397df5cbc5cbd9d
Nicholas Crane <nickcrusher30@gmail.com>
2022-10-01 11:02:25 -0400

    Refactor code for simplicity

--- grid.js
+++ grid.js
@@ -8,30 +8,44 @@ function removeAllChildren(parent) {
     }
 }
 
+// what the cell should do
+function cellFunctionality(e) {
+    const cell = e.target;
+    cell.style.backgroundColor = 'black';
+}
+
+// adds event listeners to each cell
+function addCellFunctionality() {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => cell.addEventListener('mouseover', 
+        cellFunctionality));
+}
+
 // remove all children from container and append a new grid
 function createEmptyGrid(rows, cols) {
-    // remove all children from container
+    // reset the grid
     removeAllChildren(container);
 
     // create default cell
     const cell = document.createElement('div');
     cell.style.backgroundColor = 'white';
     cell.style.borderStyle = 'dotted';
+    cell.style.borderWidth = '1px';
     cell.style.borderColor = 'black';
+    cell.style.cursor = 'pointer';
 
     // create grid of cells and append it to the container
     const grid = [...Array(16)].map(_ => Array(16).fill(null)); // matrix
     for (let i = 0; i < rows; i++)       // rows
         for (let j = 0; j < cols; j++) { // cols
             grid[i][j] = cell.cloneNode(true);
+            grid[i][j].classList.add('cell');
             container.appendChild(grid[i][j]);
         }
     container.style.gridTemplateColumns = `repeat(${cols}, auto)`;
+    addCellFunctionality();
 
     return grid;
 }
 
-
-console.log(container);
 const grid = createEmptyGrid(16, 16);
-grid[0][1].style.backgroundColor = 'black';
